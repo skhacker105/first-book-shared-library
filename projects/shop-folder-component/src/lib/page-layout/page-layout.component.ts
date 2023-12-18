71b05b96c59b7836586d17d043cf296a3880fb67
skhacker105 <skhacker105@gmail.com>
2023-12-19 00:27:38 +0530

    adding icon

--- projects/shop-folder-component/src/lib/page-layout/page-layout.component.ts
+++ projects/shop-folder-component/src/lib/page-layout/page-layout.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'lib-page-layout',
   templateUrl: './page-layout.component.html',
-  styleUrl: './page-layout.component.scss',
+  styleUrls: ['./page-layout.component.scss'],
   standalone: true,
   imports: [CommonModule]
 })
