3d27c2c866b49e504b0f337f088674902b16b3d9
skhacker105 <skhacker105@gmail.com>
2024-01-02 11:54:19 +0530

    completed basic dashboard and expansion

--- projects/shop-folder-component/src/lib/dashboard-box/dashboard-box.component.ts
+++ projects/shop-folder-component/src/lib/dashboard-box/dashboard-box.component.ts
@@ -1,18 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @Component({
   selector: 'lib-dashboard-box',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, MatBadgeModule],
   templateUrl: './dashboard-box.component.html',
   styleUrl: './dashboard-box.component.scss'
 })
 export class DashboardBoxComponent {
   @Input() title = '';
+  @Input() badge = '';
+  @Input() isExpanded = false;
   @Input() colorTheme = 'default'
 
+
   getClasses(container: string) {
-    return `bg-${this.colorTheme}-${container} text-${this.colorTheme}-${container}`;
+    return `bg-${this.colorTheme}-${container} text-${this.colorTheme}-${container} ${(this.isExpanded ? ' expanded' : '')}`;
   }
 }
