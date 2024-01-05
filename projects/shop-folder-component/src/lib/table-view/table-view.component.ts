1d01c927c282e8b94d320759ed56d54800c28cd9
skhacker105 <skhacker105@gmail.com>
2024-01-05 14:27:27 +0530

    basic routing completed between login, home, contact and main

--- projects/shop-folder-component/src/lib/table-view/table-view.component.ts
+++ projects/shop-folder-component/src/lib/table-view/table-view.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AgGridModule } from 'ag-grid-angular';
 
 @Component({
   selector: 'lib-table-view',
   standalone: true,
-  imports: [],
+  imports: [AgGridModule],
   templateUrl: './table-view.component.html',
   styleUrl: './table-view.component.scss'
 })
 export class TableViewComponent {
-
+  @Input() rowData : any[] = [
+    {make: 'Toyota', model: 'Celica', price: 35000},
+        {make: 'Ford', model: 'Mondeo', price: 32000},
+        {make: 'Porsche', model: 'Boxter', price: 72000}
+  ];
+  @Input() columnDefs : any[] = [
+    {headerName: 'Make', field: 'make'},
+    {headerName: 'Model', field: 'model'},
+    {headerName: 'Price', field: 'price', editable: true}
+  ];
+  @Input() defaultColDef : any = {};
 }
